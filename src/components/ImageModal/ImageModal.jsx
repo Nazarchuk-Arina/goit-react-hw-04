15ee309a71a3c86d2381ed6033c87d9244c48917
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2024-11-18 16:26:40 +0200

    fix

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,9 +1,18 @@
-import s from "./ImageModal.module.css";
 import Modal from "react-modal";
 
 const ImageModal = ({ image, isOpen, onRequestClose }) => {
+  const style = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={style}>
       <div>
         <img src={image.urls.regular} alt={image.alt_description} />
       </div>
